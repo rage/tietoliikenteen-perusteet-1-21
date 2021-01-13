dde213a9344a9bc9c28ddf213aaef0602de48a92
Henrik Nygren <nygrenh@gmail.com>
2021-01-13 10:51:46 +0200

    Update template

--- src/services/progress.js
+++ src/services/progress.js
@@ -3,10 +3,23 @@ import { zip } from "../util/arrays"
 import { fetchQuizProgress } from "./quiznator"
 import { fetchQuizzesProgress } from "./quizzes"
 
+const introductionCourseGroups = [
+  "osa01",
+  "osa02",
+  "osa03",
+  "osa04",
+  "osa05",
+  "osa06",
+  "osa07",
+]
+
 export async function fetchProgress(t) {
   // await fetchQuizzesProgress()
-  const serviceIdentifiers = [t("quizService")]
-  const progressesCollection = await Promise.all([fetchQuizzesProgress()])
+  const serviceIdentifiers = [t("programmingService"), t("quizService")]
+  const progressesCollection = await Promise.all([
+    fetchProgrammingProgress(),
+    fetchQuizzesProgress(),
+  ])
   const userDetails = await getCachedUserDetails()
   const currentCourseVariant = userDetails?.extra_fields?.course_variant
   const progressByGroup = {}
@@ -14,7 +27,7 @@ export async function fetchProgress(t) {
   zip(serviceIdentifiers, progressesCollection).forEach(
     ([identifier, progresses]) => {
       console.log(JSON.stringify(progresses))
-      progresses.forEach(progressEntry => {
+      progresses.forEach((progressEntry) => {
         if (!progressByGroup[progressEntry.group]) {
           progressByGroup[progressEntry.group] = {}
         }
@@ -23,8 +36,15 @@ export async function fetchProgress(t) {
     },
   )
   const toBeDeleted = []
-
-  toBeDeleted.forEach(o => {
+  Object.entries(progressByGroup).forEach(([group, serviceEntries]) => {
+    if (
+      !Object.keys(serviceEntries).find((o) => o === t("programmingService"))
+    ) {
+      toBeDeleted.push(group)
+    }
+  })
+  // TODO: this is not a good way to do this
+  toBeDeleted.forEach((o) => {
     delete progressByGroup[o]
   })
   return progressByGroup
